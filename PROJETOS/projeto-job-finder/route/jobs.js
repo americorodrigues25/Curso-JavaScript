b0e6d1d942cf16ded39471bee81d8c41eb84c2db
americorodrigues25 <americo.200422@gmail..com>
2025-08-04 19:43:16 -0300

    feat: define a rota para exibir os detalhes da vaga

--- PROJETOS/projeto-job-finder/route/jobs.js
+++ PROJETOS/projeto-job-finder/route/jobs.js
@@ -4,9 +4,21 @@ const job = require("../models/Job");
 
 // Rota de teste para verificar se o servidor está funcionando
 // Você pode acessar essa rota em http://localhost:3000/jobs/test
-router.get('/test', (req, res) => {
-    res.send("Rota funcinouuuuuuuu!"); 
+router.get("/test", (req, res) => {
+  res.send("Rota funcinouuuuuuuu!");
 });
+
+// Detalhes da vaga -> view/1, view/2, etc.
+// Define a rota para exibir os detalhes de uma vaga específica
+router.get("/view/:id", (req, res) =>
+  job
+    .findOne({
+      where: { id: req.params.id },
+    })
+    .then((job) => {
+      res.render("view", { job });
+    }).catch(error => console.error("Erro ao buscar a vaga:", error) || res.status(500).send("Erro ao buscar a vaga"))
+);
 
 // Define a rota para exibir o formulário de adição de vagas
 // Você pode acessar essa rota em http://localhost:3000/jobs/add
@@ -16,23 +28,23 @@ router.get("/add", (req, res) => {
 
 // Define a rota para obter todas as vagas
 router.post("/add", (req, res) => {
-
   let { title, salary, company, description, email, new_job } = req.body;
 
   // inserir dados no sistema
-  job.create({
-    title,
-    description, 
-    salary,
-    company,
-    email,
-    new_job,
-  })
-  .then(() => res.redirect("/")) // Redireciona para a página inicial após adicionar a vaga)
-  .catch((error) => {
-    console.error("Erro ao adicionar a vaga:", error);
-    res.status(500).send("Erro ao adicionar a vaga");
-  });
+  job
+    .create({
+      title,
+      description,
+      salary,
+      company,
+      email,
+      new_job,
+    })
+    .then(() => res.redirect("/")) // Redireciona para a página inicial após adicionar a vaga)
+    .catch((error) => {
+      console.error("Erro ao adicionar a vaga:", error);
+      res.status(500).send("Erro ao adicionar a vaga");
+    });
 });
 
 module.exports = router;
